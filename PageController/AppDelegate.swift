4ef6cfed175fcaafbe1ed0d48847abccfba25a72
Max Langer <maxlanger@me.com>
2015-09-01 21:50:13 +0200

    Initial Commit

---
+++ PageController/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  PageController
+//
+//  Created by Max Langer on 01.09.15.
+//  Copyright © 2015 Max Langer. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ PageController/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  PageController
+//
+//  Created by Max Langer on 01.09.15.
+//  Copyright © 2015 Max Langer. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
