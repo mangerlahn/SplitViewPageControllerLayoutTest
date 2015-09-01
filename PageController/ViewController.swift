02be9f6ebb8c86664e3dcd60affe371a484ca1b8
Max Langer <maxlanger@me.com>
2015-09-01 23:01:23 +0200

    First commit

--- PageController/ViewController.swift
+++ PageController/ViewController.swift
@@ -8,12 +8,23 @@
 
 import Cocoa
 
-class ViewController: NSViewController {
+class ViewController: NSViewController, NSPageControllerDelegate {
 
+    var pageController: NSPageController!
+    
+    let views = ["Split", "Split", "Split"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        self.pageController = NSPageController()
+        
+        self.pageController.delegate = self
+        self.pageController.view = self.view
+        
+        self.pageController.arrangedObjects = views
+        self.pageController.transitionStyle = NSPageControllerTransitionStyle.HorizontalStrip
     }
 
     override var representedObject: AnyObject? {
@@ -21,7 +32,21 @@ class ViewController: NSViewController {
         // Update the view, if already loaded.
         }
     }
+    
+    func pageController(pageController: NSPageController, identifierForObject object: AnyObject) -> String {
+        return object as! String
+    }
+    
+    func pageController(pageController: NSPageController, viewControllerForIdentifier identifier: String) -> NSViewController {
+        let controller = self.storyboard?.instantiateControllerWithIdentifier(identifier) as! NSViewController
+        controller.view.autoresizingMask = [.ViewHeightSizable, .ViewWidthSizable]
+        
+        return controller
+    }
 
-
+    func pageControllerDidEndLiveTransition(pageController: NSPageController) {
+        pageController.completeTransition()
+        pageController.selectedViewController!.view.autoresizingMask = [.ViewHeightSizable, .ViewWidthSizable]
+    }
 }
 
